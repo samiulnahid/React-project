16db2c7e4db01789803156dfb05a5b46d874c99f
samiulnahid <samiulislam212421@gmail.com>
2021-11-19 01:21:09 +0600

    menu complate

--- src/component/body/Menu.js
+++ src/component/body/Menu.js
@@ -1,21 +1,42 @@
 import React, {Component} from 'react';
 import DISHES from '../../data/dishes.js';
 import MenuItem from './MenuItem';
+import  DishDetails from './DishDetails';
 
 
 
 class Menu extends Component{
     state = {
-        dishes : DISHES
+        dishes : DISHES,
+        selectedDish : null
     }
+
+
+    onDishSelect = dish =>{
+
+        console.log(dish);
+        this.setState({selectedDish:dish})
+    }
+
+
 
     render(){
 
         const menu = this.state.dishes.map(item =>{
             return(
-                <MenuItem dish ={item} key={item.id} />
-            )
+                <MenuItem
+                     dish ={item} 
+                     key={item.id}
+                     DishSelect = { () => this.onDishSelect(item)} />
+            );
         })
+
+        let DishDetail = null;
+
+        if(this.state.selectedDish != null){
+
+            DishDetail = <DishDetails dish ={this.state.selectedDish}/>
+        }
 
         return(
             <div className="container">
@@ -24,6 +45,10 @@ class Menu extends Component{
 
                             {menu}
 
+                    </div>
+
+                    <div className="col-6">
+                        {DishDetail}
                     </div>
                 </div>
             </div>
