5f4c531fc507189f93e103c60c56779815eb923f
samiulnahid <samiulislam212421@gmail.com>
2021-11-18 23:29:33 +0600

    navigation bar using reactstrap

---
+++ src/component/header/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Navbar,NavbarBrand} from 'reactstrap';
+
+const Navigation = () =>{
+    return(
+        <Navbar dark color="dark">
+
+           
+            <div className="container">
+                 <NavbarBrand href="/">Restaurant Project</NavbarBrand>
+
+            </div>
+
+        </Navbar>
+    )
+}
+
+export default Navigation;
